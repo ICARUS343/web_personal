a2a3c5f9b7f37bc295b133aaece5c75d7a3f2577
ICARUS343 <hello@gmail.com>
2021-02-22 09:44:42 -0500

    Updates

---
+++ precache-manifest.81ee104bb08c2998b8476347d7140f98.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a26ba87a5d566ec4bc2abffd493f91f6",
+    "url": "/web_personal/index.html"
+  },
+  {
+    "revision": "ad651205d04a8918dd72",
+    "url": "/web_personal/static/css/2.ac1ab58b.chunk.css"
+  },
+  {
+    "revision": "ad651205d04a8918dd72",
+    "url": "/web_personal/static/js/2.5478ced7.chunk.js"
+  },
+  {
+    "revision": "9bdfc7ed727ed93f3c6e0bc546339ec1",
+    "url": "/web_personal/static/js/2.5478ced7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "132948ae1baadfb842be",
+    "url": "/web_personal/static/js/main.b4e77996.chunk.js"
+  },
+  {
+    "revision": "7163404341df965d584c",
+    "url": "/web_personal/static/js/runtime-main.83827c5c.js"
+  },
+  {
+    "revision": "ff7d8b0a557b0ef3ac7acaff3060d108",
+    "url": "/web_personal/static/media/DSC_0128.ff7d8b0a.JPG"
+  },
+  {
+    "revision": "18e5c53d49f69bdbcba53d93a3dcfffd",
+    "url": "/web_personal/static/media/DSC_0376.18e5c53d.JPG"
+  },
+  {
+    "revision": "bfa8cf37ed83199f62e6b842d65b3b62",
+    "url": "/web_personal/static/media/Resume_PS_GRP.bfa8cf37.pdf"
+  },
+  {
+    "revision": "3fed1fab00c61de829bf8d17233279b7",
+    "url": "/web_personal/static/media/homePage.3fed1fab.jpg"
+  },
+  {
+    "revision": "fb32427af5fc6f2301941d8860fd8530",
+    "url": "/web_personal/static/media/image1.fb32427a.jpg"
+  },
+  {
+    "revision": "eac191b5c4018990ae0c497fd719343c",
+    "url": "/web_personal/static/media/image2.eac191b5.JPG"
+  },
+  {
+    "revision": "41a62ebec94a7794f256f05bd5c43528",
+    "url": "/web_personal/static/media/image3.41a62ebe.JPG"
+  },
+  {
+    "revision": "cb074ba3c4036b09d55681d63e5d028f",
+    "url": "/web_personal/static/media/image4.cb074ba3.JPG"
+  }
+]);
